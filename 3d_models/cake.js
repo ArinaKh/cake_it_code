5e5307762fd70f3b43770ab78327ea6ae25736ee
Nastyaprosk <nastyaprosk13@gmail.com>
2013-12-01 12:47:31 +0400

    Добавил 3d модели

---
+++ 3d_models/cake.js
@@ -0,0 +1,38 @@
+cake = {
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "cake.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 0,
+        "faces"         : 0,
+        "normals"       : 0,
+        "colors"        : 0,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "vertices": [],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [],
+
+    "colors": [],
+
+    "uvs": [[]],
+
+    "faces": []
+
+}
---
+++ 3d_models/cyl.js
@@ -0,0 +1,38 @@
+cyl = {
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "cilinder.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 0,
+        "faces"         : 0,
+        "normals"       : 0,
+        "colors"        : 0,
+        "uvs"           : 0,
+        "materials"     : 0
+    },
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 255*255*255 +255*255 + 0*1,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "vertices": [],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [],
+
+    "colors": [],
+
+    "uvs": [[]],
+
+    "faces": []
+
+}
